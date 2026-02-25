2673c5dbae57c96f081778aa72acb04994bd8254
Matthew Trappett <mtrappet@uoregon.edu>
2026-02-24 16:13:18 -0800

    revamped cnn, ready to start changing it over to cnn

--- binary_classification/models.py
+++ binary_classification/models.py
@@ -43,34 +43,34 @@ class TwoLayerMLP(nn.Module):
     
 
 class CNN(nn.Module):
-    """A simple CNN model."""
-    hidden_dim: int
+    """A simple CNN model using setup for shared layers."""
+    hidden_dim: int  
     output_dim: int = 1
 
     def setup(self):
-        self.layer1 = nn.Dense(features=self.hidden_dim, use_bias=False)
-        self.layer2 = nn.Dense(features=self.output_dim, use_bias=False)
+        self.conv1 = nn.Conv(features=8, kernel_size=(3, 3))
+        self.conv2 = nn.Conv(features=16, kernel_size=(3, 3))
+        self.conv3 = nn.Conv(features=32, kernel_size=(3, 3))
+        self.dense1 = nn.Dense(features=128)
+        self.classifier = nn.Dense(features=self.output_dim)
 
-    @nn.compact
-    def __call__(self, x):
-        x = nn.Conv(features=32, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-        x = nn.relu(x)
-        x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = x.reshape((x.shape[0], -1))  # flatten
-        x = self.layer1(x)
-        x = nn.relu(x)
-        x = self.layer2(x)
-        return x
-    
     def get_features(self, x: jax.Array) -> jax.Array:
-        """
-        Returns the hidden representation (post-ReLU).
-        """
-        x = self.layer1(x)
-        x = nn.relu(x)
+        x = nn.relu(self.conv1(x))
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(3, 3))
+        
+        x = nn.relu(self.conv2(x))
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(3, 3))
+        
+        x = nn.relu(self.conv3(x))
+        x = nn.max_pool(x, window_shape=(3, 3), strides=(3, 3))
+        
+        x = jnp.mean(x, axis=(1, 2)) # Global average pooling
+        x = nn.relu(self.dense1(x))
+        return x
+
+    def __call__(self, x: jax.Array) -> jax.Array:
+        x = self.get_features(x)
+        x = self.classifier(x)
         return x
     
 
