b672357c4212515c614c3cad0784a2694589f5a5
Matthew Trappett <mtrappet@uoregon.edu>
2026-04-06 10:23:25 -0700

    removed constraints talapas_test.py

--- binary_classification/run_scripts/talapas_test.sh
+++ binary_classification/run_scripts/talapas_test.sh
@@ -10,7 +10,7 @@
 #SBATCH --ntasks=1             ### Number of tasks per array job
 #SBATCH --ntasks-per-node=1   ### Nuber of tasks to be launched per Node
 #SBATCH --gpus=1              ### General Reservation of gpu:number of gpus
-#SBATCH --constraint="[gpu-40gb|gpu-80gb|2xgpu-80gb|3xgpu-80gb]"
+
 
 #SBATCH --account=tau  ### Account used for job submission
 
