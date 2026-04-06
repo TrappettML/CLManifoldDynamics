0f7c1371322f659f553bc672d54ec3728235445d
Matthew Trappett <mtrappet@uoregon.edu>
2026-04-06 10:36:45 -0700

    update t test

--- binary_classification/run_scripts/talapas_test.sh
+++ binary_classification/run_scripts/talapas_test.sh
@@ -10,14 +10,13 @@
 #SBATCH --ntasks=1             ### Number of tasks per array job
 #SBATCH --ntasks-per-node=1   ### Nuber of tasks to be launched per Node
 #SBATCH --gpus=1              ### General Reservation of gpu:number of gpus
-
-
+#SBATCH --gres=a100,gpu-80gb,2xgpu-80gb,gpu-40gb
 #SBATCH --account=tau  ### Account used for job submission
-
-#SBATCH --mail-type=all
-#SBATCH --mail-user=mtrappet@uoregon.edu
-
 #SBATCH --array=0-4           ### Array index
+
+module load cuda/13.0
+
+conda activate talapas_mandi_env
 
 echo "=========================================================="
 echo "Job Info: Slurm job ${SLURM_JOB_ID}, array job ${SLURM_ARRAY_JOB_ID}, task ${SLURM_ARRAY_TASK_ID}"
