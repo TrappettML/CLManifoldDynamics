5c950f9de1b3ad11c8665eca31ff76fa0d93b5b5
Matthew Trappett <mtrappet@uoregon.edu>
2026-04-08 18:53:50 -0700

    array jobs

--- binary_classification/run_scripts/talapas_single_run.sh
+++ binary_classification/run_scripts/talapas_single_run.sh
@@ -14,7 +14,7 @@
 #SBATCH --mem=160G
 
 #SBATCH --account=tau  ### Account used for job submission
-#SBATCH --array=0-3           ### Array index
+#SBATCH --array=0-1           ### Array index
 
 module load cuda/13.0
 module load miniconda3/20240410
